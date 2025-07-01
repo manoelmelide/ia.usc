398bcbd8c87002de8411ee7f9b66d5fecf9983bf
manoelmelide <manoel.melide@gmail.com>
2025-07-01 17:40:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import HorarioGrid from './components/HorarioGrid';
 import ParcialesCalendar from './components/ParcialesCalendar';
 import ExamenesCalendar from './components/ExamenesCalendar';
 
-function App() {
+export default function App() {
   return (
-    <div style={{ padding: '1rem' }}>
-      <h1>Grao en Intelixencia Artificial USC</h1>
+    <div style={{ padding: 20 }}>
+      <h1>Grao en Intelixencia Artificial – USC</h1>
       <HorarioGrid />
+      <hr />
       <ParcialesCalendar />
+      <hr />
       <ExamenesCalendar />
     </div>
   );
 }
-
-export default App;
