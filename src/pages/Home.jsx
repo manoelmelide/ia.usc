e55309b3a72f10eef94c77d1c6f7ab3a13068bbe
manoelmelide <manoel.melide@gmail.com>
2025-07-02 13:55:05 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+/* Home.jsx */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Grao en Intelixencia Artificial – USC</h1>
+      <Link to="/gestion">Acceder a Gestión</Link>
+    </div>
+  );
+}
