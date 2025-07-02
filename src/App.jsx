797f0b3cfe9632ac3235384011dfbbcd68a331aa
manoelmelide <manoel.melide@gmail.com>
2025-07-02 14:12:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,16 @@
 /* src/App.jsx */
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import netlifyIdentity from 'netlify-identity-widget';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/Login';
 import Gestion from './pages/Gestion';
 
-// Componente que protege rutas
-function PrivateRoute({ children, user }) {
-  return user ? children : <Navigate to="/login" replace />;
-}
-
 export default function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    netlifyIdentity.init();
-    netlifyIdentity.on('login', u => {
-      setUser(u);
-      netlifyIdentity.close();
-    });
-    netlifyIdentity.on('logout', () => setUser(null));
-    const current = netlifyIdentity.currentUser();
-    if (current) setUser(current);
-
-    return () => {
-      netlifyIdentity.off('login');
-      netlifyIdentity.off('logout');
-    };
-  }, []);
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login netlifyIdentity={netlifyIdentity} />} />
-        <Route
-          path="/gestion"
-          element={
-            <PrivateRoute user={user}>
-              <Gestion user={user} />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/gestion" element={<Gestion />} />
       </Routes>
     </Router>
   );
 }
-
