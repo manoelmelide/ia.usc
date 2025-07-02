8938d356f2efe78abb3d3fd50b9f2bbb460e35fd
manoelmelide <manoel.melide@gmail.com>
2025-07-02 13:56:22 +0200

    Create Gestion.jsx

---
+++ src/pages/Gestion.jsx
@@ -0,0 +1,12 @@
+/* Gestion.jsx */
+import React from 'react';
+
+export default function Gestion({ user }) {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Zona de Gestión</h2>
+      <p>Bienvenido, {user.user_metadata.full_name || user.email}</p>
+      {/* Aquí pones tu interfaz de gestión */}
+    </div>
+  );
+}
